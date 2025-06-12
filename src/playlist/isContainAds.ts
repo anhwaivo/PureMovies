eef8928838e056a76f50b81337b0936c536f86be
Thành Hoàng Trần <hth4nh@gmail.com>
2025-06-12 14:39:24 +0700

    Fix regex lastIndex reset (#3)

--- src/playlist/isContainAds.ts
+++ src/playlist/isContainAds.ts
@@ -1,5 +1,8 @@
 import { config } from "../misc/state";
 
 export function isContainAds(playlist: string) {
-    return config.adsRegexList.some((regex) => regex.test(playlist));
+    return config.adsRegexList.some((regex) => {
+        regex.lastIndex = 0;
+        return regex.test(playlist);
+    });
 }
