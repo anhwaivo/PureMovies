11ca64495add7c200cc32d0005cd967eeb64ec6e
Hth4nh <hth4nh@gmail.com>
2025-06-14 14:04:25 +0700

    Update getlink mechanism (nguonc)

--- src/playlist/getPlaylistURL.ts
+++ src/playlist/getPlaylistURL.ts
@@ -1,29 +1,38 @@
-import { config } from "../misc/state";
 import { unrestrictedFetch } from "../network";
 
 export async function getPlaylistURL(embedUrl: string | URL) {
     // Parse the input URL
     embedUrl = new URL(embedUrl);
 
+    // kkphim
     if (embedUrl.hostname.includes("phimapi") && embedUrl.searchParams.has("url")) {
         return embedUrl.searchParams.get("url") ?? "";
     }
 
-    const isNoNeedToBypass = config.domainBypassWhitelist.some((keyword) =>
-        embedUrl.hostname.includes(keyword)
-    );
+    // ophim
+    if (embedUrl.hostname.includes("opstream")) {
+        const req = await fetch(embedUrl);
+        const raw = await req.text();
 
-    // Fetch the content of the URL
-    const req = isNoNeedToBypass
-        ? await fetch(embedUrl)
-        : await unrestrictedFetch(embedUrl, {
+        const playlistUrl = raw.match(/(?<=const url = ").*(?=";)/)?.[0];
+        return URL.parse(String(playlistUrl), embedUrl)?.href || "";
+    }
+
+    // nguonc
+    if (embedUrl.hostname.includes("streamc")) {
+        const req = await unrestrictedFetch(embedUrl, {
             headers: {
                 Referer: embedUrl.origin,
             },
         });
 
-    const raw = await req.text();
+        const raw = await req.text();
 
-    const playlistUrl = raw.match(/(?<=(?:url =|file:) ").*(?="(?:;|,))/)?.[0];
-    return URL.parse(String(playlistUrl), embedUrl)?.href || "";
+        const encryptedURL = raw.match(/(?<=encryptedURL = ").*(?=";)/)?.[0];
+        const playlistUrl = `conf.php?url=${encodeURIComponent(String(encryptedURL))}`
+        return URL.parse(playlistUrl, embedUrl)?.href || "";
+    }
+
+    // For other cases, return the original embed URL
+    return embedUrl.href;
 }
