dc6b4c291b1d1e1d8a4ff1d3906bc91b7664a472
Hth4nh <hth4nh@gmail.com>
2025-06-16 11:52:59 +0700

    Update getlink mechanism (nguonc)

--- src/playlist/getPlaylistURL.ts
+++ src/playlist/getPlaylistURL.ts
@@ -18,7 +18,6 @@ export async function getPlaylistURL(embedUrl: string | URL) {
         return URL.parse(String(playlistUrl), embedUrl)?.href || "";
     }
 
-    // nguonc
     if (embedUrl.hostname.includes("streamc")) {
         const req = await unrestrictedFetch(embedUrl, {
             headers: {
@@ -28,9 +27,22 @@ export async function getPlaylistURL(embedUrl: string | URL) {
 
         const raw = await req.text();
 
+        // Extract the encrypted URL from the raw response
         const encryptedURL = raw.match(/(?<=encryptedURL = ").*(?=";)/)?.[0];
-        const playlistUrl = `conf.php?url=${encodeURIComponent(String(encryptedURL))}`
-        return URL.parse(playlistUrl, embedUrl)?.href || "";
+        if (encryptedURL) {
+            const playlistUrl = `conf.php?url=${encodeURIComponent(encryptedURL)}`;
+            return URL.parse(playlistUrl, embedUrl)?.href || "";
+        }
+
+        // If no encrypted URL is found, try to find the stream URL
+        const streamURL = raw.match(/(?<=(?:streamURL =|url =|file:) ").*(?="(?:;|,))/)?.[0];
+        if (streamURL) {
+            const playlistUrl = JSON.parse(`"${streamURL}"`);
+            return URL.parse(playlistUrl, embedUrl)?.href || "";
+        }
+
+        // If all above fails, return the embed URL with "get.php" instead of "embed.php"
+        return embedUrl.href.replace("embed.php", "get.php");
     }
 
     // For other cases, return the original embed URL
